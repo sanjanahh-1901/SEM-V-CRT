0e0a318fbe6fe284d6c5209b3d5304a044b817b8
Sanjana Bobbidy <2401100cse0107@cse.suh.edu.in>
2026-08-06 16:16:09 +0530

    Refactor comments for Leetcode Problem 1480 and add missing newline at the end of the file

--- Practice/M01_Sliding_Window_&_Two_Pointers/S08/Prefix_Optimization_Process.py
+++ Practice/M01_Sliding_Window_&_Two_Pointers/S08/Prefix_Optimization_Process.py
@@ -1,8 +1,8 @@
+'''
 #Leetcode Problem: 1480. Running Sum of 1d Array
-'''
- Input: nums = [1,2,3,4]
- Output: [1,3,6,10]
-'''
+Input: nums = [1,2,3,4]
+Output: [1,3,6,10]
+
 #Solution 
 nums = [1,2,3,4]
 res = [0] * (len(nums))  #O(n)
@@ -18,9 +18,17 @@ nums = [1,2,3,4]
 for i in range(1, len(nums)):
     nums[i] += nums[i-1]   #O(n)
 print(nums)
+'''
 
 #Leetcode Problem: 1732. Find the Highest Altitude
 '''
 Input: gain = [-5,1,5,0,-7]
 Output: 1       
 '''
+class Solution:
+    def largestAltitude(self, gain: List[int]) -> int:
+        n = len(gain)
+        alt = [0] * (n + 1)  #O(n)
+        for i in range(n):
+            alt[i + 1] = alt[i] + gain[i]  #O(n)
+        return max(alt)  #O(n)
