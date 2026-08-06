4e1a39894cb48d67317275ea0eeb75caf78bb175
Sanjana Bobbidy <2401100cse0107@cse.suh.edu.in>
2026-08-06 15:58:35 +0530

    Add solution for Leetcode Problem 1480: Running Sum of 1d Array

---
+++ Practice/M01_Sliding_Window_&_Two_Pointers/S08/Prefix_Optimization_Process.py
@@ -0,0 +1,26 @@
+#Leetcode Problem: 1480. Running Sum of 1d Array
+'''
+ Input: nums = [1,2,3,4]
+ Output: [1,3,6,10]
+'''
+#Solution 
+nums = [1,2,3,4]
+res = [0] * (len(nums))  #O(n)
+for i in range(len(nums)):
+    curr_sum = 0
+    for j in range(i+1):      #O(n^2)
+        curr_sum += nums[j]
+    res[i] = curr_sum
+print(res)
+
+#Optimal Solution
+nums = [1,2,3,4]    
+for i in range(1, len(nums)):
+    nums[i] += nums[i-1]   #O(n)
+print(nums)
+
+#Leetcode Problem: 1732. Find the Highest Altitude
+'''
+Input: gain = [-5,1,5,0,-7]
+Output: 1       
+'''
