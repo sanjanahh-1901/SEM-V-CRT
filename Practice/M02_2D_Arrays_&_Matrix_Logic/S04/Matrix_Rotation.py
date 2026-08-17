400492172243d80b878a21e397a56ccd6b6de219
Sanjana Bobbidy <2401100cse0107@cse.suh.edu.in>
2026-08-17 16:12:01 +0530

    Refactor rotate method to use self parameter and improve formatting; add example usage

--- Practice/M02_2D_Arrays_&_Matrix_Logic/S04/Matrix_Rotation.py
+++ Practice/M02_2D_Arrays_&_Matrix_Logic/S04/Matrix_Rotation.py
@@ -1,5 +1,19 @@
 #Leetcode 48: Rotate Image
+#Solution using two loops
+from typing import List
 class Solution:
-    def rotate(self, matrix: List[List[int]]) -> None:
+    def rotate(matrix: List[List[int]]) -> None:
         n = len(matrix)
-        
+        # Transpose the matrix
+        for i in range(n):
+            for j in range(i, n):
+                matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+        # Reverse each row
+        for i in range(n):
+            matrix[i].reverse() 
+        return matrix
+matrix = [[1,2,3],[4,5,6],[7,8,9]]
+print(Solution.rotate(matrix)) #Output: [[7,4,1],[8,5,2],[9,6,3]]
+
+#Leetcode 1886: Determine Whether Matrix Can Be Obtained By Rotation
+#Solution using two loops
